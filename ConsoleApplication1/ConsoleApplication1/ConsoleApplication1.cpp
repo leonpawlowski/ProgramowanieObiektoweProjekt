40dc25d0368d681d7f7b06548c0f1305aafbecb1
leonpawlowski <287089@student.pwr.edu.pl>
2025-03-24 16:45:35 +0100

    laby4_zmiana_na_klasy

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,29 +1,6 @@
 ﻿#include <iostream>
 
-bool czyWPamieciZnajdujeSieWartosc = false;
-bool czyZakonczycProgram = false;
-double mem;
-
-double dodawanie(double a, double b) {
-    return a + b;
-}
-
-double odejmowanie(double a, double b) {
-    return a - b;
-}
-
-double mnozenie(double a, double b) {
-    return a * b;
-}
-
-double dzielenie(double a, double b) {
-    return a / b;
-}
-
-double resztaZDzielenia(double a, double b) {
-    return a - (int)(a / b) * b;
-}
-
+/*
 int konwersjaSystemowLiczbowychOd1Do9(int podstawa, int liczba) {
     int licznik = 1, wynik = 0;
     while (liczba > 0) {
@@ -64,95 +41,118 @@ void konwersjaSystemowLiczbowych() {
     mem = liczba;
     czyWPamieciZnajdujeSieWartosc = true;
 }
+*/
 
-void czyszczeniePamieci() {
-    czyWPamieciZnajdujeSieWartosc = false;
-}
-
-void wypiszMenu() {
-    std::cout << "Menu" << '\n';
-    std::cout << "1) +" << '\n';
-    std::cout << "2) -" << '\n';
-    std::cout << "3) *" << '\n';
-    std::cout << "4) /" << '\n';
-    std::cout << "5) %" << '\n';
-    std::cout << "6) konwersja systemow liczbowych dla liczb calkowitych" << '\n';
-    std::cout << "7) wyczysc m" << '\n';
-    std::cout << "8) wyjdz" << '\n';
-}
-
-void wyborIObslugaOpcji() {
-    std::cout << "wybor: ";
-    int wybor;
-    std::cin >> wybor;
-    double a, b;
-    if (wybor < 6 && wybor>0) {
-        if (!czyWPamieciZnajdujeSieWartosc) {
+class Kalkulator {
+    bool czyWPamieciZnajdujeSieWartosc = false;
+    double mem;
+public:
+    bool czyZakonczycProgram = false;
+    void wypiszMenu() {
+        std::cout << "Menu" << '\n';
+        if (czyWPamieciZnajdujeSieWartosc)
+            std::cout << "wartosc w pamieci: " << mem << '\n';
+        std::cout << "1) +" << '\n';
+        std::cout << "2) -" << '\n';
+        std::cout << "3) *" << '\n';
+        std::cout << "4) /" << '\n';
+        std::cout << "5) %" << '\n';
+        std::cout << "6) konwersja systemow liczbowych dla liczb calkowitych" << '\n';
+        std::cout << "7) wyczysc m" << '\n';
+        std::cout << "8) operacje na pamieci" << '\n';
+        std::cout << "0) wyjdz" << '\n';
+    };
+    void wyborIObslugaOpcji() {
+        std::cout << "wybor: ";
+        int wybor;
+        std::cin >> wybor;
+        double a, b;
+        if (wybor < 6 && wybor>0) {
             std::cout << "podaj pierwsza liczbe: ";
             std::cin >> a;
             std::cout << "podaj druga liczbe: ";
             std::cin >> b;
         }
-        else {
-            std::cout << "pierwsza liczba z pamieci: " << mem << '\n';
+        else if (wybor == 8) {
+            std::cout << "wybierz operacje (1-6): ";
+            std::cin >> wybor;
+            std::cout << "liczba w pamieci: " << mem<<'\n';
+            a = mem;
             std::cout << "podaj druga liczbe: ";
             std::cin >> b;
-            a = mem;
         }
-    }
-    switch (wybor) {
-    case 1:
-        mem = dodawanie(a, b);
-        std::cout << "wynik: " << mem << '\n';
-        czyWPamieciZnajdujeSieWartosc = true;
-        break;
-    case 2:
-        mem = odejmowanie(a, b);
-        czyWPamieciZnajdujeSieWartosc = true;
-        std::cout << "wynik: " << mem << '\n';
-        break;
-    case 3:
-        mem = mnozenie(a, b);
-        std::cout << "wynik: " << mem << '\n';
-        czyWPamieciZnajdujeSieWartosc = true;
-        break;
-    case 4:
-        if (b != 0) {
-            mem = dzielenie(a, b);
+        switch (wybor) {
+        case 0:
+            czyZakonczycProgram = true;
+            break;
+        case 1:
+            dodawanie(a, b);
             std::cout << "wynik: " << mem << '\n';
             czyWPamieciZnajdujeSieWartosc = true;
-        }
-        else
-            std::cout << "DZIELENIE PRZEZ ZERO!!!" << '\n';
-        break;
-    case 5:
-        if (b != 0) {
-            mem = resztaZDzielenia(a, b);
+            break;
+        case 2:
+            odejmowanie(a, b);
+            czyWPamieciZnajdujeSieWartosc = true;
+            std::cout << "wynik: " << mem << '\n';
+            break;
+        case 3:
+            mnozenie(a, b);
             std::cout << "wynik: " << mem << '\n';
             czyWPamieciZnajdujeSieWartosc = true;
-        }
-        else
-            std::cout << "DZIELENIE PRZEZ ZERO!!!" << '\n';
-        break;
-    case 6:
-        konwersjaSystemowLiczbowych();
-        break;
-    case 7:
-        czyszczeniePamieci();
-        break;
-    case 8:
-        czyZakonczycProgram = true;
-        break;
-    default:
-        break;
+            break;
+        case 4:
+            if (b != 0) {
+                dzielenie(a, b);
+                std::cout << "wynik: " << mem << '\n';
+                czyWPamieciZnajdujeSieWartosc = true;
+            }
+            else
+                std::cout << "DZIELENIE PRZEZ ZERO!!!" << '\n';
+            break;
+        case 5:
+            if (b != 0) {
+                resztaZDzielenia(a, b);
+                std::cout << "wynik: " << mem << '\n';
+                czyWPamieciZnajdujeSieWartosc = true;
+            }
+            else
+                std::cout << "DZIELENIE PRZEZ ZERO!!!" << '\n';
+            break;
+        case 6:
+            //konwersjaSystemowLiczbowych();
+            break;
+        case 7:
+            czyszczeniePamieci();
+            break;
+        default:
+            break;
+        };
     }
-}
-
+    void dodawanie(double a, double b) {
+        mem = a + b;
+    };
+    void odejmowanie(double a, double b) {
+        mem = a - b;
+    };
+    void mnozenie(double a, double b) {
+        mem = a * b;
+    };
+    void dzielenie(double a, double b) {
+        mem = a / b;
+    };
+    void resztaZDzielenia(double a, double b) {
+        mem = a - (int)(a / b) * b;
+    };
+    void czyszczeniePamieci() {
+        czyWPamieciZnajdujeSieWartosc = false;
+    };
+};
 
 int main() {
-    while (!czyZakonczycProgram) {
-        wypiszMenu();
-        wyborIObslugaOpcji();
+    Kalkulator kalkulator1;
+    while (!kalkulator1.czyZakonczycProgram) {
+        kalkulator1.wypiszMenu();
+        kalkulator1.wyborIObslugaOpcji();
     }
     return 0;
 }
